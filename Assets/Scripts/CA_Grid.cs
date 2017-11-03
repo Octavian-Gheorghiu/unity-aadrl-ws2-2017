f6027c53400b835fcc5684cb361edba20fee1eed
Octavian Gheorghiu <octavian.gheorghiu@yahoo.com>
2017-11-03 00:52:21 +0000

    Session 5
    Added the capacity to record a CA.

--- Assets/Scripts/CA_Grid.cs
+++ Assets/Scripts/CA_Grid.cs
@@ -9,16 +9,21 @@ public class CA_Grid : MonoBehaviour {
 	// Texture to be used as start of CA input
 	public Texture2D seedImage;
 
+	// Time frame
+	public int timeEnd = 100;
+	int currentFrame = 0;
+
 	// Array for storing voxels
 	int width;
 	int length;
-	GameObject[,] voxelGrid;
+	int height;
+	GameObject[,,] voxelGrid;
 
 	// Reference to the voxel we are using
 	public GameObject voxelPrefab;
 
 	// Spacing between voxels
-	float spacing = 1.2f;
+	float spacing = 1.0f;
 
 	// FUNCTIONS
 
@@ -27,60 +32,90 @@ public class CA_Grid : MonoBehaviour {
 		// Read the image width and height
 		width = seedImage.width;
 		length = seedImage.height;
+		height = timeEnd;
 		// Create a new CA grid
 		CreateGrid ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// Calculate the future state of the voxels
-		CalculateCA ();
-		// Update the voxels and display them
-		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < length; j++) {
-				GameObject currentVoxel = voxelGrid [i, j];
-				currentVoxel.GetComponent<Voxel> ().Update ();
-				currentVoxel.GetComponent<Voxel> ().DisplayVoxel ();
-			}
-		}		
-	}
+        // Increment time frame
+        if (currentFrame < timeEnd - 1)
+        {
+            // Calculate the future state of the voxels
+            CalculateCA();
+            // Update the voxels that are printing
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    GameObject currentVoxel = voxelGrid[i, j, 0];
+                    currentVoxel.GetComponent<Voxel>().UpdateVoxel();
+                }
+            }
+            // Save the CA state
+            SaveCA();
+            currentFrame++;
+        }
+
+        // Display the printed voxels
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < length; j++)
+            {
+                for (int k = 1; k < height; k++)
+                {
+                    voxelGrid[i, j, k].GetComponent<Voxel>().VoxelDisplay();
+                }
+            }
+        }
+    }
 
 	// Create grid function
 	void CreateGrid(){
 		// Allocate space in memory for the array
-		voxelGrid = new GameObject[width, length];
+		voxelGrid = new GameObject[width, length, height];
 		// Populate the array with voxels from a base image
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < length; j++) {
-				// Create values for the transfrom of the new voxel
-				Vector3 currentVoxelPos = new Vector3 (i*spacing,0,j*spacing);
-				Quaternion currentVoxelRot = Quaternion.identity;
-				GameObject currentVoxel = Instantiate (voxelPrefab, currentVoxelPos, currentVoxelRot);
-				// Create a new state based on the input image
-				int currentVoxelState = (int)seedImage.GetPixel(i,j).grayscale;
-				// Set the state of the new voxel
-				currentVoxel.GetComponent<Voxel> ().SetState (currentVoxelState);
-				// Attach the new voxel to the grid game object
-				currentVoxel.transform.parent = gameObject.transform;
-				// Save the current voxel in the voxelGrid array
-				voxelGrid[i,j] = currentVoxel;
+				for (int k = 0; k < height; k++) {
+					// Create values for the transform of the new voxel
+					Vector3 currentVoxelPos = new Vector3 (i*spacing,k*spacing,j*spacing);
+					Quaternion currentVoxelRot = Quaternion.identity;
+					GameObject currentVoxel = Instantiate (voxelPrefab, currentVoxelPos, currentVoxelRot);
+                    currentVoxel.GetComponent<Voxel>().SetupVoxel();
+                    // Set the state of the voxels
+                    if (k == 0) {						
+						// Create a new state based on the input image
+						int currentVoxelState = (int)seedImage.GetPixel (i, j).grayscale;
+						currentVoxel.GetComponent<Voxel> ().SetState (currentVoxelState);
+					} else {
+                        // Set the state to death
+                        currentVoxel.GetComponent<MeshRenderer>().enabled = false;
+                        currentVoxel.GetComponent<Voxel> ().SetState (0);
+					}
+					// Save the current voxel in the voxelGrid array
+					voxelGrid[i,j,k] = currentVoxel;
+					// Attach the new voxel to the grid game object
+					currentVoxel.transform.parent = gameObject.transform;
+				}
 			}
 		}
 	}
 
-	// Caluclate CA function
+	// Calculate CA function
 	void CalculateCA(){
 		// Go over all the voxels stored in the voxels array
 		for (int i = 1; i < width-1; i++) {
 			for (int j = 1; j < length-1; j++) {
-				GameObject currentVoxel = voxelGrid[i,j];
+				GameObject currentVoxel = voxelGrid[i,j,0];
 				int currentVoxelState = currentVoxel.GetComponent<Voxel> ().GetState ();
 				int aliveNeighbours = 0;
 
-				// Calculate how many alive neighbours are oround the current voxel
+				// Calculate how many alive neighbours are around the current voxel
 				for (int x = -1; x <= 1; x++) {
 					for (int y = -1; y <= 1; y++) {
-						GameObject currentNeigbour = voxelGrid [i + x, j + y];
+						GameObject currentNeigbour = voxelGrid [i + x, j + y,0];
 						int currentNeigbourState = currentNeigbour.GetComponent<Voxel> ().GetState();
 						aliveNeighbours += currentNeigbourState;
 					}
@@ -92,7 +127,7 @@ public class CA_Grid : MonoBehaviour {
 					if (aliveNeighbours < 2) {
 						currentVoxel.GetComponent<Voxel> ().SetFutureState (0);
 					}
-					// If there are two or three neigbours alive I am going to stay alive
+					// If there are two or three neighbours alive I am going to stay alive
 					if(aliveNeighbours == 2 || aliveNeighbours == 3){
 						currentVoxel.GetComponent<Voxel> ().SetFutureState (1);
 					}
@@ -108,6 +143,23 @@ public class CA_Grid : MonoBehaviour {
 						currentVoxel.GetComponent<Voxel> ().SetFutureState (1);
 					}
 				}
+			}
+		}
+	}
+
+	void SaveCA(){
+		for(int i =0; i< width; i++){
+			for (int j = 0; j < length; j++) {
+                GameObject currentVoxel = voxelGrid[i, j, 0];
+                int currentVoxelState = currentVoxel.GetComponent<Voxel>().GetState();
+                // Save the voxel state
+                GameObject savedVoxel = voxelGrid[i, j, currentFrame];
+                savedVoxel.GetComponent<Voxel> ().SetState (currentVoxelState);                
+                // Save the voxel age if voxel is alive
+                if (currentVoxelState == 1) {
+                    int currentVoxelAge = currentVoxel.GetComponent<Voxel>().GetAge();
+                    savedVoxel.GetComponent<Voxel>().SetAge(currentVoxelAge);
+                }
 			}
 		}
 	}
--- Assets/Scripts/Voxel.cs
+++ Assets/Scripts/Voxel.cs
@@ -5,34 +5,57 @@ using UnityEngine;
 public class Voxel : MonoBehaviour {
 
 	// VARIABLES
-	int state = 0;
-	int futureState = 0;
+	public int state = 0;
+    private int futureState = 0;
+    public int age = 0;
+    int timeEndReference;
+    private MaterialPropertyBlock props;
 
-	// FUNCTIONS
+    // FUNCTIONS
+
+    public void SetupVoxel()
+    {
+        timeEndReference = GameObject.Find("CA_Grid").GetComponent<CA_Grid>().timeEnd;
+        props = new MaterialPropertyBlock();
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
 	
 	// Update function
-	public void Update () {
-		state = futureState;
-	}
+	public void UpdateVoxel () {
+		// Set the future state
+		state = futureState;        
+        // If voxel is alive update age
+        if (state == 1)
+        {
+            age++;
+        }
+        // If voxel is death disable the game object mesh renderer and set age to zero
+        if (state == 0)
+        {
+            age = 0;
+        }
+    }
 
-	// Display the voxel
-	public void DisplayVoxel(){
-		MaterialPropertyBlock props = new MaterialPropertyBlock ();
-		MeshRenderer renderer;
-		if (state == 0) {
-			// Disable the renderer
-			renderer = gameObject.GetComponent<MeshRenderer> ();
-			renderer.enabled = false;
-		}
-		if (state == 1) {
-			// Enable the renderer
-			renderer = gameObject.GetComponent<MeshRenderer> ();
-			renderer.enabled = true;
-			// Set the color to black
-			props.SetColor("_Color", Color.black);
-			renderer.SetPropertyBlock (props);
-		}
-	}
+    // Update the voxel display
+    public void VoxelDisplay()
+    {
+        // Get the game object mesh renderer
+        MeshRenderer renderer;
+        renderer = gameObject.GetComponent<MeshRenderer>();
+        if (state == 1)
+        {            
+            // Remap the color to age
+            Color ageColor = new Color(Remap(age, 0, timeEndReference, 0.0f, 1.0f), 0, 0, 1);
+            props.SetColor("_Color", ageColor);
+            // Updated the mesh renderer color
+            renderer.enabled = true;
+            renderer.SetPropertyBlock(props);
+        }
+        if(state == 0)
+        {
+            renderer.enabled = false;
+        }
+    }
 
 	// Set the state of the voxel
 	public void SetState(int _state){
@@ -44,8 +67,24 @@ public class Voxel : MonoBehaviour {
 		futureState = _futureState;
 	}
 
+    // Get the age of the voxel
+	public void SetAge(int _age){
+		age = _age;
+	}
+
 	// Get the state of the voxel
 	public int GetState(){
 		return state;
 	}
+
+	// Get the age of the voxel
+	public int GetAge(){
+		return age;
+	}
+
+    // Remap numbers
+    private float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
 }
